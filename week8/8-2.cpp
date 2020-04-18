b6f03411d7041e7f277cb3d8433525b93d807882
GongHailong <1034231958@qq.com>
2020-04-18 17:45:37 +0800

    add 8-2

---
+++ week8/8-2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<sstream>
+#include<cstring>
+using namespace std;
+
+template <class T>
+class Array 
+{
+  	enum { sz = 5 };
+  	T i[sz];
+	public:
+  		Array() { memset(i, 0, sz* sizeof(*i)); }
+  		T& operator[](int x) 
+		{
+    		if(x<0||x>=sz) cout << "Array::operator[] out of range" << endl;
+    		return i[x];
+  		}
+  		friend ostream& operator<<(ostream& os, const Array<T>& ia)
+  		{
+  			for(int j = 0; j < ia.sz; j++) 
+  			{
+    			os << ia.i[j];
+    			if(j != ia.sz -1)	os << ", ";
+  			}
+  			os << endl;
+  			return os;
+		}
+  		friend istream& operator>>(istream& is, Array<T>& ia)
+  		{
+  			for(int j = 0; j < ia.sz; j++)
+    		is >> ia.i[j];
+  			return is;
+		}
+};
+
+int main() 
+{
+	//‘⁄Dev-cpp…œ±‡“Î
+  	stringstream input("47 34 56 92 103");
+ 	Array<int>  a;
+    input >> a;
+    a[4] = -1;           // Use overloaded operator[]
+    cout << a;
+} 
